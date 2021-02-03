a5d94c6a70335558b566a9f78e16e67a0993d53e
Deniz Caglar <denizcaglar200@gmail.com>
2021-02-03 12:55:04 -0600

    commented out docker installation check, looking into implementing it in a better way

--- build_cont.sh
+++ build_cont.sh
@@ -4,14 +4,12 @@ if [ "$EUID" -ne 0 ]
 	then echo "Please run as root" 
 	exit
 fi 
-if [ "$(docker -v)" -ne 0 ] 
-	then echo "Please verify your docke installation" 
-	exit 
-fi 
+#if [ "$(docker -v)" -ne 0 ] 
+#	then echo "Please verify your docker installation" 
+#	exit 
+#fi 
 
 sudo systemctl start docker  
-
-#docker volume create 225-vol 
 
 docker build -t 225-container . 
 
